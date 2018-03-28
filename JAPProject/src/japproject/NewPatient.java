02c8b03d10d40b7123086f3e40f4cd796205fd47
Jesus Fonseca <jefh15@hotmail.com>
2018-03-28 10:47:53 -0600

    save to pull, no changes

--- JAPProject/src/japproject/NewPatient.java
+++ JAPProject/src/japproject/NewPatient.java
@@ -108,7 +108,7 @@ public class NewPatient {
     private iLabel lbl_HorarioPaciente;//Lbl para la el Horario del Paciente
     private iLabel lbl_DetalleHorarioPaciente;//Lbl para el Detalle Horario del Paciente
     private iTextField txt_DetalleHorarioPaciente;//TextField para el Detalle Horario del Paciente
-    private iLabel lbl_TipoPaciente;//Lbl para el Tipo de Paciente
+    private iLabel lbl_TipoPaciente;//Lbl para el Tipo de Paciente 
     //FIN de Controles Swing para Paciente
     
     public NewPatient(iFrame if_) {
@@ -118,7 +118,6 @@ public class NewPatient {
         Ingresar();
         
     }
-    
     
     /**
      * Método que crea los componentes en el RePanel de Paciente
