38bb9baef0570658687b843f8ecea022fc5c58cd
JPGomez12 <37844627+JPGomez12@users.noreply.github.com>
2018-03-30 12:44:06 -0600

    Maintenance_tipo_paciente

--- JAPProject/src/japproject/Maintenance_tPaciente.java
+++ JAPProject/src/japproject/Maintenance_tPaciente.java
@@ -33,9 +33,9 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 
-public class Maintenance_Horario {
+public class Maintenance_tPaciente {
   
-    public iPanel Maintenance_Horario_Panel;//creo el iPanel
+    public iPanel Maintenance_TipPaciente_Panel;//creo el iPanel
     
     private final String DATABASE_URL = "jdbc:mysql://icomponents.net:3306/icompone_jose";
     private final String USERNAME = "icompone_jose";
@@ -48,15 +48,15 @@ public class Maintenance_Horario {
     private iButton btnEliminar;//Boton para eliminar
     
     private iLabel lbl_Titulo_Mantenimiento;//Lbl para el Titulo de la pagina de mantenimiento en la que se encuentra
-    private iLabel lbl_idHorario;//Lbl para el id curso
-    private iTextField txt_idHorario;//TextField para el id curso
-    private iLabel lbl_NombreHorario;//Lbl para el nombre del curso
-    private iTextField txt_NombreHorario;//TextField para el nombre del curso
+    private iLabel lbl_idTipoPaciente;//Lbl para el id curso
+    private iTextField txt_idTipPaciente;//TextField para el id curso
+    private iLabel lbl_NombreTipoPaciente;//Lbl para el nombre del curso
+    private iTextField txt_NombreTipoPaciente;//TextField para el nombre del curso
     
-    public Maintenance_Horario(iFrame if_) {
-        currentPanel = "Maintenance_Horario";  //Assign the value of currentPanel for RemovePanels method which handles panel transitions.   
-        Maintenance_Horario_Panel = new iPanel(0, 70, 100.0f, 100.0f, 0, 0, if_);//le doy propiedades al iPanel
-        Maintenance_Horario_Panel.setBackground(new java.awt.Color(00, 52, 25));//le doy color al panel
+    public Maintenance_tPaciente(iFrame if_) {
+        currentPanel = "Maintenance_tPaciente";  //Assign the value of currentPanel for RemovePanels method which handles panel transitions.   
+        Maintenance_TipPaciente_Panel = new iPanel(0, 70, 100.0f, 100.0f, 0, 0, if_);//le doy propiedades al iPanel
+        Maintenance_TipPaciente_Panel.setBackground(new java.awt.Color(00, 52, 25));//le doy color al panel
         Mantenimiento_curso(if_);
     }
     
@@ -66,17 +66,17 @@ public class Maintenance_Horario {
         lbl_LogoPsicologia = new iLabel("");
         lbl_LogoPsicologia.setIcon(new javax.swing.ImageIcon(getClass().getResource("/content/LOGO DE PSICOLOGIA.PNG")));
          
-        lbl_Titulo_Mantenimiento = new iLabel("MANTENIMIENTO HORARIOS");
+        lbl_Titulo_Mantenimiento = new iLabel("MANTENIMIENTO TIPO PACIENTE");
         lbl_Titulo_Mantenimiento.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         lbl_Titulo_Mantenimiento.setForeground(Color.GRAY.brighter());
         
-        lbl_idHorario = new iLabel("ID Horario".toUpperCase());
-        lbl_idHorario.setForeground(Color.GRAY.brighter());
-        txt_idHorario = new iTextField("", 3);
+        lbl_idTipoPaciente = new iLabel("ID Tipo Paciente".toUpperCase());
+        lbl_idTipoPaciente.setForeground(Color.GRAY.brighter());
+        txt_idTipPaciente = new iTextField("", 3);
         
-        lbl_NombreHorario = new iLabel("Nombre Horario".toUpperCase());
-        lbl_NombreHorario.setForeground(Color.GRAY.brighter());
-        txt_NombreHorario = new iTextField("", 3);
+        lbl_NombreTipoPaciente = new iLabel("Nombre Tipo Paciente".toUpperCase());
+        lbl_NombreTipoPaciente.setForeground(Color.GRAY.brighter());
+        txt_NombreTipoPaciente = new iTextField("", 3);
         
         btnAñadir = new iButton("Añadir", 2, Color.GRAY, Color.BLACK);//boton para añadir horario
         btnModificar = new iButton("Modificar", 2, Color.GRAY, Color.BLACK);//boton para editar horario
@@ -85,44 +85,44 @@ public class Maintenance_Horario {
     private void Mantenimiento_curso(iFrame if_) {
         Componentes_Curso();
         
-        Maintenance_Horario_Panel.AddObject(lbl_LogoULatina, 415, 120, 10);
-        Maintenance_Horario_Panel.AddObject(lbl_LogoPsicologia, 415, 120, 440);//añade los logos oficiales de la clinica y de la universidad latina
-        Maintenance_Horario_Panel.newLine(); 
+        Maintenance_TipPaciente_Panel.AddObject(lbl_LogoULatina, 415, 120, 10);
+        Maintenance_TipPaciente_Panel.AddObject(lbl_LogoPsicologia, 415, 120, 440);//añade los logos oficiales de la clinica y de la universidad latina
+        Maintenance_TipPaciente_Panel.newLine(); 
  
-        Maintenance_Horario_Panel.AddObject(lbl_Titulo_Mantenimiento, 415, 30, 5);//agrego el titulo
-        Maintenance_Horario_Panel.newLine();
+        Maintenance_TipPaciente_Panel.AddObject(lbl_Titulo_Mantenimiento, 415, 30, 5);//agrego el titulo
+        Maintenance_TipPaciente_Panel.newLine();
         
-        Maintenance_Horario_Panel.AddObject(lbl_idHorario, 415, 30, 4);
-        Maintenance_Horario_Panel.AddObject(txt_idHorario, 415, 30, 200);//agrega el label y el textfield del id horario
-        Maintenance_Horario_Panel.newLine();
+        Maintenance_TipPaciente_Panel.AddObject(lbl_idTipoPaciente, 415, 30, 4);
+        Maintenance_TipPaciente_Panel.AddObject(txt_idTipPaciente, 415, 30, 200);//agrega el label y el textfield del id horario
+        Maintenance_TipPaciente_Panel.newLine();
         
-        Maintenance_Horario_Panel.AddObject(lbl_NombreHorario, 415, 30, 4);
-        Maintenance_Horario_Panel.AddObject(txt_NombreHorario, 415, 30, 200);//agrega el label y el textfield del nombre del horario
-        Maintenance_Horario_Panel.newLine();
+        Maintenance_TipPaciente_Panel.AddObject(lbl_NombreTipoPaciente, 415, 30, 4);
+        Maintenance_TipPaciente_Panel.AddObject(txt_NombreTipoPaciente, 415, 30, 200);//agrega el label y el textfield del nombre del horario
+        Maintenance_TipPaciente_Panel.newLine();
         
-        Maintenance_Horario_Panel.AddObject(btnAñadir, 130, 30, 200);
+        Maintenance_TipPaciente_Panel.AddObject(btnAñadir, 130, 30, 200);
         btnAñadir.addActionListener((a) -> {
             btnAñadir_MouseClicked();
         });
         
-        Maintenance_Horario_Panel.AddObject(btnModificar, 130, 30, 340);
+        Maintenance_TipPaciente_Panel.AddObject(btnModificar, 130, 30, 340);
         btnModificar.addActionListener((a) -> {
             btnModificar_MouseClicked();
         });
         
-        Maintenance_Horario_Panel.AddObject(btnEliminar, 130, 30, 480);
+        Maintenance_TipPaciente_Panel.AddObject(btnEliminar, 130, 30, 480);
         btnEliminar.addActionListener((a) -> {
             btnEliminar_MouseClicked(); 
         });
-        Maintenance_Horario_Panel.newLine();
+        Maintenance_TipPaciente_Panel.newLine();
         
         Tabla();
         
-        if_.add(Maintenance_Horario_Panel);
+        if_.add(Maintenance_TipPaciente_Panel);
     } 
     
     public void Tabla(){
-        ArrayList<String> cols = new ArrayList<>(Arrays.asList("ID Horario", "Nombre Horario"));
+        ArrayList<String> cols = new ArrayList<>(Arrays.asList("ID Tipo Paciente", "Nombre Tipo Paciente"));
         iPanel ip = new iPanel(215, 90, 500, 400, 4);
         //iPanel ip = new iPanel(0, 70, 50.0f, 50.0f, 0, 0, if_);
         ip.setLocation(100, 300);
@@ -130,14 +130,14 @@ public class Maintenance_Horario {
         ip.setBackground(Color.black);
 
         ResultSet rs = sql.SELECT(""
-                + "SELECT `IdHorario`, `NombreHorario` "
-                + "FROM `JAW_Horario`");
+                + "SELECT `IdTipoPaciente`, `NombreTipoPaciente` "
+                + "FROM `JAW_TipoPaciente`");
         if (sql.Exists(rs)) {
             try {
                 while (rs.next()) {
                     Object[] result = {
-                        rs.getObject("IdHorario"),
-                        rs.getObject("NombreHorario")
+                        rs.getObject("IdTipoPaciente"),
+                        rs.getObject("NombreTipoPaciente")
                     };
                     table.addrow(result);
                 }
@@ -150,16 +150,16 @@ public class Maintenance_Horario {
 
         ip.add(scrollPane);
         ip.finalice(); 
-        Maintenance_Horario_Panel.add(ip);
+        Maintenance_TipPaciente_Panel.add(ip);
     }
     
     public void btnAñadir_MouseClicked(){
         try{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-        PreparedStatement pps = con.prepareStatement("INSERT INTO JAW_Horario(NombreHorario) VALUES (?)");
+        PreparedStatement pps = con.prepareStatement("INSERT INTO JAW_TipoPaciente(NombreTipoPaciente) VALUES (?)");
         
-        pps.setString(1 , txt_NombreHorario.getText());
+        pps.setString(1 , txt_NombreTipoPaciente.getText());
         
         pps.executeUpdate();
         Tabla();  //Esto se supone que debe de actualizar la pagina
@@ -172,10 +172,10 @@ public class Maintenance_Horario {
         try{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-        PreparedStatement pps = con.prepareStatement("UPDATE JAW_Horario SET NombreHorario=? WHERE IdHorario=?");
+        PreparedStatement pps = con.prepareStatement("UPDATE JAW_TipoPaciente SET NombreTipoPaciente=? WHERE IdTipoPaciente=?");
         
-        pps.setString(1 , txt_NombreHorario.getText());
-        pps.setString(2 , txt_idHorario.getText());
+        pps.setString(1 , txt_NombreTipoPaciente.getText());
+        pps.setString(2 , txt_idTipPaciente.getText());
         
         pps.executeUpdate();
         Tabla();
@@ -188,9 +188,9 @@ public class Maintenance_Horario {
         try{
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
-        PreparedStatement pps = con.prepareStatement("DELETE FROM `JAW_Horario` where `IdHorario` = ?");
+        PreparedStatement pps = con.prepareStatement("DELETE FROM `JAW_TipoPaciente` where `IdTipoPaciente` = ?");
         
-        pps.setString(1 , txt_idHorario.getText());
+        pps.setString(1 , txt_idTipPaciente.getText());
         
         pps.executeUpdate();
         Tabla();
