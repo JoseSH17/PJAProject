96c198f921cd8e2c28ba0b4d2603976c9d354f96
JoseSH17 <josesh17@gmail.com>
2018-04-01 11:09:58 -0600

    Appointments update
    Appointments update

--- JAPProject/src/japproject/Appointments.java
+++ JAPProject/src/japproject/Appointments.java
@@ -11,6 +11,7 @@ import iComponents.iLabel;
 import iComponents.iPanel;
 import iComponents.iScrollPane;
 import iComponents.iTable;
+import iComponents.iTextField;
 import static japproject.HomePanel.currentPanel;
 import static japproject.JAPProject.sql;
 import java.awt.Color;
@@ -26,6 +27,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JButton;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JTable;
@@ -38,38 +40,55 @@ public class Appointments {
 
     public iPanel Appointments_Panel;
     public iLabel lblCalendar;
-
+    public iTextField txtHiddenSearch;
     public iCalendar calendar;
-
+    public JButton btnViewAll;
     public iTable tblCitas;
 
     public List<String> tbl_Data = new ArrayList();
 
     public Appointments(iFrame if_) {
-        try {
+        try {                        
             currentPanel = "Appointments_Panel";  //Assign the value of currentPanel for RemovePanels method which handles panel transitions.   
             Appointments_Panel = new iPanel(0, 70, if_.getWidth(), 100.0f, 0, 0, if_);//Defining iPanel dimensions
             Appointments_Panel.setBackground(new java.awt.Color(00, 52, 25));//le doy color al panel               
 
-            lblCalendar = new iLabel("Por favor Seleccione una fecha: "); //Lbl Guide
+            lblCalendar = new iLabel("Por favor seleccione una fecha: "); //Lbl Guide
             lblCalendar.setForeground(Color.WHITE); //Calendar
             calendar = new iCalendar();
-
+            
+            txtHiddenSearch = new iTextField("",0); //Hidden txt that searches in table and updates result for used depending on selected date, this is not shown to the user.           
+            
             CalendarUpdateTable(calendar); //Calling method to update table dynamically
-
+                       
+            
+            CheckFirstExecution(txtHiddenSearch);
+            
+            btnViewAll = new JButton("Ver todas las citas");
+            btnViewAll.setToolTipText("Elimina el filtro y muestra todas las citas");
+            
+            Appointments_Panel.addSpace(20); //Leaving space from top
+            
             Appointments_Panel.AddObject(lblCalendar, 190, 30, 3);
             Appointments_Panel.AddObject(calendar, 70, 30, 193);
+            Appointments_Panel.AddObject(btnViewAll, 140, 30, 303);
             Appointments_Panel.newLine();
+            
+             btnViewAll.addActionListener((ae) -> {
+                 txtHiddenSearch.setText(""); //Deletes filter defined on txtHiddenSearch and show all data
+                 
+             });
+            
 
-            ResultSet rs = sql.SELECT("SELECT * FROM JAW_VistaCitas");
+            ResultSet rs = sql.SELECT("SELECT * FROM JAW_VistaCitas order by `Fecha Cita` desc");
 
             ArrayList<String> Cols = new ArrayList();
             for (int i = 1; i < rs.getMetaData().getColumnCount() + 1; i++) {
                 Cols.add(rs.getMetaData().getColumnName(i));
             }
 //          Se crea la tabla y se le da los parametros
-
-            tblCitas = new iTable(Cols);
+            System.out.println("Valor de txthiddensearch:" + txtHiddenSearch.getText());
+            tblCitas = new iTable(Cols, txtHiddenSearch);
             tblCitas.getTableHeader().setReorderingAllowed(false);
             tblCitas.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
             tblCitas.getTableHeader().setResizingAllowed(false);
@@ -105,17 +124,28 @@ public class Appointments {
         }
     }
 
+    public void CheckFirstExecution(iTextField check)
+    {   
+        if(check.getText().isEmpty())
+        {
+            Date date =  new Date(); //Getting current date from local host
+            DateFormat currentDateFormatted = new SimpleDateFormat("yyyy/MM/dd"); //Formatting current date for initial search and table refresh            
+            txtHiddenSearch.setText(currentDateFormatted.format(calendar.getDate()));
+        }
+    }
+    
     public void CalendarUpdateTable(iCalendar calendar) {
         calendar.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                calendarActionListener(calendar.getDate());
+                calendarActionListener(calendar.getDate());                
             }
         });
     }
 
     public void calendarActionListener(Date date) {
-        DateFormat currentDateFormatted = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        DateFormat currentDateFormatted = new SimpleDateFormat("yyyy/MM/dd");
+        txtHiddenSearch.setText(currentDateFormatted.format(date));        
         System.out.println("Fecha Actual: " + currentDateFormatted.format(date));
     }
 
