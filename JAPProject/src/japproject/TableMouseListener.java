c9761b020f79149e854e5d58b9e8e19d32a7e867
anferny20 <anferny1999@gmail.cm>
2018-04-06 18:44:13 -0600

    123

--- JAPProject/src/japproject/TableMouseListener.java
+++ JAPProject/src/japproject/TableMouseListener.java
@@ -1,29 +1,37 @@
 package japproject;
- 
+
 import java.awt.Point;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
- 
+
 import javax.swing.JTable;
- 
+
 /**
- * A mouse listener for a JTable component.
- * tuma
+ * A mouse listener for a JTable component. tuma
  *
  */
 public class TableMouseListener extends MouseAdapter {
-     
+
     private JTable table;
-     
+    public int currentRow;
+
     public TableMouseListener(JTable table) {
         this.table = table;
     }
-     
+
     @Override
     public void mousePressed(MouseEvent event) {
         // selects the row at which point the mouse is clicked
         Point point = event.getPoint();
-        int currentRow = table.rowAtPoint(point);
-        table.setRowSelectionInterval(currentRow, currentRow);
+        currentRow = -1;
+        currentRow = table.rowAtPoint(point);
+        if (currentRow == -1) {
+            System.out.println("jesus se la come");
+
+        }else{
+            table.setRowSelectionInterval(currentRow, currentRow);
+        }
+       
+
     }
 }
